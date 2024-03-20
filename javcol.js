bdd6dd720671da070ba4a7144d5548078a950559
Obunwa Jephthah <jephthahobunwa@gmail.com>
2024-03-20 12:18:47 +0100

    a color generator

---
+++ javcol.js
@@ -0,0 +1,8 @@
+function generateColor() {
+  var color = '#' + Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = color;
+}
+
+function resetColor() {
+  document.body.style.backgroundColor = '';
+}
